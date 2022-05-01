6f070ed3c7a1f21cc54d6dae7df85ad4d33f3354
SERIGNE BASSIROU DIAMOU SERIGNE TOUBA NDIAYE <dstndiaye@gmail.com>
2022-05-01 14:02:02 +0000

    Configuration du Middlewere e suppression

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const stuffRoutes = require('./routes/stuff');
-
 const userRoutes = require('./routes/user');
+
 
 const app = express();
 
@@ -32,8 +32,6 @@ app.use((req, res, next) => {
   next();
 });
 
-
-app.use('/api/stuff', stuffRoutes);
 
 app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoutes);
